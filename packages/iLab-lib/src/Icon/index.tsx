371310a270d48c87d7ce43a18054890bf618aa05
maimai <15867193547@163.com>
2022-03-09 23:59:56 +0800

    fix: icon添加urls扩展

--- packages/iLab-lib/src/Icon/index.tsx
+++ packages/iLab-lib/src/Icon/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { createFromIconfontCN } from '@ant-design/icons'
 
-const IconFont = createFromIconfontCN({
-  scriptUrl: ['//at.alicdn.com/t/font_2601113_v4hbrtqz9s.js'],
-})
 
 interface Props {
   type: string
+  urls?: string[],
   style?: React.CSSProperties
   className?: string
   rotate?: number
@@ -18,5 +16,10 @@ interface Props {
 }
 
 export default function Icon(props: Props): JSX.Element {
+  const { urls = [] } = props;
+const IconFont = createFromIconfontCN({
+  scriptUrl: urls.concat(['//at.alicdn.com/t/font_2601113_v4hbrtqz9s.js']),
+})
+
   return <IconFont {...props} />
 }
